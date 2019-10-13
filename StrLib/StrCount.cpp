d080a1d7f1954282ac558fc48c881188ab6dc516
BOND12 <56501627+BOND12@users.noreply.github.com>
2019-10-13 11:35:30 +0300

    Add files via upload

---
+++ StrLib/StrCount.cpp
@@ -0,0 +1,21 @@
+/*
+ * StrCount.cpp
+ *
+ *  Created on: 5 янв. 2019 г.
+ *      Author: BOND
+ */
+
+
+#include "StrCount.h"
+#include <iostream>
+using namespace std;
+
+
+char ms[ccd];
+
+
+void leh(){
+	setlocale (LC_ALL,"Russian");
+	cout << "privet"<<endl;
+}
+
